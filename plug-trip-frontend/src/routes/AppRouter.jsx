bf46019972e7342a6c424f7d04d26a4bf83d737b
zmxn9941@naver.com <zmxn9941@naver.com>
2026-08-03 15:45:49 +0900

    feature : 라우터 설정중

--- plug-trip-frontend/src/routes/AppRouter.jsx
+++ plug-trip-frontend/src/routes/AppRouter.jsx
@@ -1,12 +1,19 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import HomePage from "../pages/homePage/HomePage.jsx";
+import { Route, Routes } from "react-router-dom";
+import RootLayout from "../components/layout/RootLayout.jsx";
+import HomePage from "../pages/homePage/HomePage";
 
 export default function AppRouter() {
   return (
-    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route element={<RootLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/explore" element={<h1>여행 탐색</h1>} />
+          <Route path="/planner" element={<h1>AI 여행 만들기</h1>} />
+          <Route path="/my-trips" element={<h1>내 여행</h1>} />
+        </Route>
+
+        <Route path="/login" element={<h1>로그인</h1>} />
+        <Route path="*" element={<h1>페이지를 찾을 수 없습니다.</h1>} />
       </Routes>
-    </BrowserRouter>
-    );
+  );
 }
