55c573ca2d2ddccee2a6095ace7eba6a190971e2
zmxn9941@naver.com <zmxn9941@naver.com>
2026-08-03 11:55:33 +0900

    기본 틀

---
+++ plugTrip_back/src/main/java/com/my/plugTrip_back/PlugTripBackApplication.java
@@ -0,0 +1,13 @@
+package com.my.plugTrip_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlugTripBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlugTripBackApplication.class, args);
+	}
+
+}
---
+++ plugTrip_back/src/test/java/com/my/plugTrip_back/PlugTripBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.plugTrip_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlugTripBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
