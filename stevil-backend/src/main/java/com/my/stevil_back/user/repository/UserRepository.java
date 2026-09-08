aba8bfe842a81c7f51cf7c8d22ae7a55bfdd793b
zmxn9941@naver.com <zmxn9941@naver.com>
2026-09-08 17:13:07 +0900

    fix : 레포지토리 구조 해결 병합에서 사라진 내용 복구

--- stevil-backend/src/main/java/com/my/stevil_back/user/repository/UserRepository.java
+++ stevil-backend/src/main/java/com/my/stevil_back/user/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.my.stevil_back.user.repository;
 
 import com.my.stevil_back.user.entity.User;
 import com.my.stevil_back.user.entity.enumType.UserRole;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -39,5 +41,15 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Page<User> searchForAdmin(
             @Param("keyword") String keyword,
             Pageable pageable
+    );
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("""
+        SELECT u
+        FROM User u
+        WHERE u.id = :id
+        """)
+    Optional<User> findByIdForUpdate(
+            @Param("id") Long id
     );
 }
